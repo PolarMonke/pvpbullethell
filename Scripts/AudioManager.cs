05b0aaf060f4845673446af0c349401c6a13c955
PolarMonke <polarmonkey000@gmail.com>
2025-03-12 10:51:33 +0300

    Synced soundtracks

--- Scripts/AudioManager.cs
+++ Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 public partial class AudioManager : Node2D
 {
@@ -8,10 +9,21 @@ public partial class AudioManager : Node2D
 	List<AudioStreamMP3> _audioSources = new List<AudioStreamMP3>();
 	public override void _Ready()
 	{
+		_audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
 		_audioSources.Add(GD.Load<AudioStreamMP3>("res://Sounds/soundtrack1.mp3"));
 		_audioSources.Add(GD.Load<AudioStreamMP3>("res://Sounds/soundtrack2.mp3"));
 		_audioSources.Add(GD.Load<AudioStreamMP3>("res://Sounds/soundtrack3.mp3"));
-		_audioPlayer.Stream = _audioSources[new Random().Next(0,2)];
+		if (GetMultiplayerAuthority() == 1)
+		{
+			int index = new Random().Next(0,2);
+			Rpc(nameof(PlaySoundtrack), index);
+		}
+	}
+	
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	private void PlaySoundtrack(int index)
+	{
+		_audioPlayer.Stream = _audioSources[index];
 		_audioPlayer.Play();
 	}
 
